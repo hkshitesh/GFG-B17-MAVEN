508359c5076bdd1525b0dd9dfa0bdc1e020c350a
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-07 21:40:37 +0530

    added Dockerfile

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,17 @@
+package mypkg;
+
+public class MyClass {
+	
+	public int sum(int a, int b)
+	{	
+		int c;
+		int d;
+		c=10+15;
+		return c;
+	}
+
+	public static void main(String[] args) {		
+		MyClass ob = new MyClass();
+		System.out.println(ob.sum(20,30));
+	}
+}
